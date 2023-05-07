c8870aa394d0a83f7de3b5d0f5eee62e1e540600
cmcnult3 <cmcnult3@email.cpcc.edu>
2023-05-07 10:50:50 -0400

    Update unp.php

--- joyphp/src/unp.php
+++ joyphp/src/unp.php
@@ -1,7 +1,7 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "cpses_ces5kd83qn@localhost";       //"cmcnulty"; "cpses_ces5kd83qn@localhost"
-$password = "Wcunc1adybuG$";   //"web250"; "Wcunc1adybuG$"
+$dbhost = "mi3-ss122.a2hosting.com";
+$dbuser = "cemcnult";       //"cmcnulty"; "cpses_ces5kd83qn@localhost"
+$password = "D5#q3GYP-jf4p2";   //"web250"; "Wcunc1adybuG$"
 
 ?>
